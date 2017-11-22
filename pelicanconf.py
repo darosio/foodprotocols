6c8c47bf431d467d5bdae2dbd72f196eb6005801
daniele arosio <daniele.arosio@cnr.it>
2017-11-22 10:31:28 +0100

    octopress theme and org_pandoc_reader

--- pelicanconf.py
+++ pelicanconf.py
@@ -19,6 +19,8 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
+THEME = '../pelican-octopress-theme/'
+
 # Blogroll
 # LINKS = (('Pelican', 'http://getpelican.com/'),
          # ('Python.org', 'http://python.org/'),
@@ -30,6 +32,11 @@ AUTHOR_FEED_RSS = None
           # ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
+
+PLUGIN_PATHS = ['../pelican-plugins']
+PLUGINS = ['org_pandoc_reader']
+
+# ORG_READER_EMACS_LOCATION = '/usr/bin/emacs'
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
