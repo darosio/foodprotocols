1c1d894a476d4bcd66b084d5f7689e2574947ee9
daniele arosio <daniele.arosio@cnr.it>
2017-11-22 16:04:10 +0100

    add github user atom and search menu

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'daniele arosio'
 SITENAME = 'Food protocols'
+SITESUBTITLE = u'il cibo che cucino'
 SITEURL = ''
 
 PATH = 'content'
@@ -49,3 +50,32 @@ PLUGINS = ['org_pandoc_reader', 'summary',
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+
+# RSS/Atom feeds
+FEED_DOMAIN = SITEURL
+FEED_ATOM = 'atom.xml'
+
+
+# Search
+SEARCH_BOX = True
+
+
+#Github include settings
+GITHUB_USER = 'darosio'
+GITHUB_REPO_COUNT = 1
+GITHUB_SKIP_FORK = True
+GITHUB_SHOW_USER_LINK = True
+
+# Sharing
+# TWITTER_USER = 'jakevdp'
+# GOOGLE_PLUS_USER = 'jakevdp'
+# GOOGLE_PLUS_ONE = True
+# GOOGLE_PLUS_HIDDEN = False
+# FACEBOOK_LIKE = False
+# TWITTER_TWEET_BUTTON = True
+# TWITTER_LATEST_TWEETS = True
+# TWITTER_FOLLOW_BUTTON = True
+# TWITTER_TWEET_COUNT = 3
+# TWITTER_SHOW_REPLIES = 'false'
+# TWITTER_SHOW_FOLLOWER_COUNT = 'true'
