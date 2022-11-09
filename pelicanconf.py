eab8252b343522fb4fff1c99e28d6cd415b810cb
daniele arosio <daniele.arosio@cnr.it>
2022-11-09 14:08:51 +0100

    updating

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,17 +1,13 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*- #
-from __future__ import unicode_literals
+AUTHOR = "daniele arosio"
+SITENAME = "Food protocols"
+SITESUBTITLE = "il cibo che cucino"
+SITEURL = "https://darosio.github.io/foodprotocols"
 
-AUTHOR = 'daniele arosio'
-SITENAME = 'Food protocols'
-SITESUBTITLE = u'il cibo che cucino'
-SITEURL = 'https://darosio.github.io/foodprotocols'
+PATH = "content"
 
-PATH = 'content'
+TIMEZONE = "Europe/Rome"
 
-TIMEZONE = 'Europe/Paris'
-
-DEFAULT_LANG = 'it'
+DEFAULT_LANG = "it"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -21,61 +17,36 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-# LINKS = (('Pelican', 'http://getpelican.com/'),
-         # ('Python.org', 'http://python.org/'),
-         # ('Jinja2', 'http://jinja.pocoo.org/'),
-         # ('You can modify those links in your config file', '#'),)
+LINKS = (
+    ("Pelican", "https://getpelican.com/"),
+    ("Python.org", "https://www.python.org/"),
+    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
+    ("You can modify those links in your config file", "#"),
+)
 
 # Social widget
-# SOCIAL = (('You can add links in your config file', '#'),
-          # ('Another social link', '#'),)
+SOCIAL = (
+    ("You can add links in your config file", "#"),
+    ("Another social link", "#"),
+)
 
 DEFAULT_PAGINATION = 10
 
-# Title menu options
-MENUITEMS = [('Archives', 'archives.html')]
-
-STATIC_PATHS = ['images', 'figures', 'downloads']
-
-THEME = 'pelican-octopress-theme/'
-
-PLUGIN_PATHS = ['../pelican-plugins']
-PLUGINS = ['org_pandoc_reader', 'summary',
-           'liquid_tags.include_code',
-           'liquid_tags.literal',
-           'render_math']
-
-
-# ORG_READER_EMACS_LOCATION = '/usr/bin/emacs'
-
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+# RELATIVE_URLS = True
 
+# Title menu options
+MENUITEMS = [("Archives", "archives.html")]
 
-# RSS/Atom feeds
-FEED_DOMAIN = SITEURL
-FEED_ATOM = 'atom.xml'
+STATIC_PATHS = ["images", "figures", "downloads"]
 
+THEME = "pelican-octopress-theme/"
 
-# Search
-SEARCH_BOX = True
-
-
-#Github include settings
-GITHUB_USER = 'darosio'
-GITHUB_REPO_COUNT = 0
-GITHUB_SKIP_FORK = True
-GITHUB_SHOW_USER_LINK = True
-
-# Sharing
-TWITTER_USER = 'danielearosio'
-GOOGLE_PLUS_USER = 'danielepietroarosio'
-GOOGLE_PLUS_ONE = True
-GOOGLE_PLUS_HIDDEN = False
-# FACEBOOK_LIKE = False
-TWITTER_TWEET_BUTTON = True
-TWITTER_LATEST_TWEETS = True
-TWITTER_FOLLOW_BUTTON = True
-TWITTER_TWEET_COUNT = 3
-# TWITTER_SHOW_REPLIES = 'false'
-TWITTER_SHOW_FOLLOWER_COUNT = 'true'
+PLUGIN_PATHS = ["../pelican-plugins"]
+PLUGINS = [
+    "org_pandoc_reader",
+    "summary",
+    "liquid_tags.include_code",
+    "liquid_tags.literal",
+    "render_math",
+]
